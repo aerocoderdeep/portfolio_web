a34e87b24b7f260c4d0fdb1ad33e784ced209c95
Amandeep Singh <amandeepoct97@gmail.com>
2025-05-05 04:56:27 -0700

    Update Aman_web_resume.py

--- Aman_web_resume.py
+++ Aman_web_resume.py
@@ -504,7 +504,7 @@ def Skill_Set():
                 st.error("Background image not found. Please check the file path.")
 
     # ✅ Apply background image (Make sure the path is correct)
-    set_background("D:/Education work/Amantah Education/AEROSPACE/soft-hard-skills.png")
+    set_background("oft-hard-skills.png")
     
     st.header("Technical Skills")
     col1, col2, col3 = st.columns(3)

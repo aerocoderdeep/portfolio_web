5e85e192532e9d39222b7d4b0e9dfe413b819401
Amandeep Singh <amandeepoct97@gmail.com>
2025-07-19 23:51:51 -0700

    Update Aman_web_resume.py

--- Aman_web_resume.py
+++ Aman_web_resume.py
@@ -1132,7 +1132,7 @@ def welcome_page():
                     "title": "Aerospace Web Page (Self)",
                     "date": "April 2025",
                     "desc": "Created an educational web platform for aerospace enthusiasts and learners.",
-                    "link": "https://demoaerospacewebpage-dcwmrxpaq8vap2b5pxqbvj.streamlit.app/"
+                    "link": "website: https://demoaerospacewebpage-dcwmrxpaq8vap2b5pxqbvj.streamlit.app/"
                 },
                 {
                     "title": "Aerodynamic Parameter Calculator (Self)",
